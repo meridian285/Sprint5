c0fee4733300bc173d4c17cf6fc4738debe6fa08
Aleksandr Elizarov <akelizarovav@gmail.com>
2022-09-19 06:55:47 +0300

    64dytfygj

---
+++ src/main/java/AnimalFarm.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class AnimalFarm {
+    private List<String> farmAnimals;
+
+    public AnimalFarm(List<String> farmAnimals) {
+        this.farmAnimals = farmAnimals;
+    }
+
+    public Map<Animal, Integer> countedAnimals(String farmAnimals){
+        Map<Animal, Integer> amountAnimals = new HashMap();
+        return amountAnimals;
+    }
+}
